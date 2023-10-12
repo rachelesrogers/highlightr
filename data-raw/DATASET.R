ed4959bd8d00c71c884558408cabd1f3fa69e872
rachelrogers-gbr <rachel.rogers@huskers.unl.edu>
2023-10-12 18:12:24 -0500

    adding datasets

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,8 +2,14 @@
 
 library(readr)
 library(dplyr)
-comment_full<- read_csv("~/PhD Research/comment_example_highlightr.csv")
+comment_full<- read_csv("~/PhD Research/ResponseType_Survey/hybrid_response_dataset.csv")
 comment_example <- comment_full %>% filter(page_count==2 & page_notes !="") %>%
   select(clean_prints, page_notes) %>% rename("ID"="clean_prints", "Notes"="page_notes")
 
 usethis::use_data(comment_example, overwrite = TRUE)
+
+transcript_full <- read_csv("~/PhD Research/ResponseType_Survey/Combined_Testimony_Formatted.csv")
+
+transcript_example <- transcript_full %>% filter(Page==2) %>% select(Text)
+
+usethis::use_data(transcript_example, overwrite = TRUE)
