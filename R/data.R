73e2955cff19224ef885f28f8e2e4c31b045fb8c
rachelrogers-gbr <rachel.rogers@huskers.unl.edu>
2024-01-23 12:51:21 -0600

    documenting data

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' Comment Example Dataset
+#'
+#' Participant comments for the initial description used in the jury perception
+#' study
+#'
+#'
+#' @format ## `validation_dataset`
+#' A data frame with 561 rows and 4 columns:
+#' \describe{
+#'   \item{clean_prints}{Identifier}
+#'   \item{page_count}{page number}
+#'   \item{notes}{The uncleaned notes}
+#'   \item{corrected_notes}{hand cleaned notes to demonstrate correct format}
+#'
+#' }
+#' @source Jury Perception Study
+"comment_example"
+
+
+#' Transcript Example
+#'
+#' Text corresponding to participant comments
+#'
+#'
+#' @format ## `validation_dataset`
+#' A data frame with 561 rows and 4 columns:
+#' \describe{
+#'   \item{clean_prints}{Identifier}
+#'   \item{page_count}{page number}
+#'   \item{notes}{The uncleaned notes}
+#'   \item{corrected_notes}{hand cleaned notes to demonstrate correct format}
+#'
+#' }
+#' @source Jury Perception Study
+"transcript_example"
