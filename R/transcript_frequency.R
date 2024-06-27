357a83afc3a625f7851bd727998c10e0f970a1b2
rachelrogers-gbr <rachel.rogers@huskers.unl.edu>
2024-06-27 12:34:36 -0500

    ensure space after >

--- R/transcript_frequency.R
+++ R/transcript_frequency.R
@@ -50,6 +50,7 @@ transcript_cleaning <- function(transcript){
   poem$lines <- gsub("/"," ",  poem$lines)
   poem$lines <- gsub(" <", "<", poem$lines)
   poem$lines <- gsub("< ", "<", poem$lines)
+  poem$lines <- gsub(">", "> ", poem$lines)
   # poem$lines <- gsub("<.*?>", "", poem$lines)
   # poem$lines <- poem$lines  %>% stringr::str_replace("<center>", "")  %>%
   #   stringr::str_replace("---","")
