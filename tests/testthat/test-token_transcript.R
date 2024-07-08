66c815718615c64483440b35eb83d5db0103e8b7
rachelrogers-gbr <rachel.rogers@huskers.unl.edu>
2024-07-08 15:03:06 -0500

    lowercasing in transcript token

---
+++ tests/testthat/test-token_transcript.R
@@ -0,0 +1,10 @@
+test_that("html tags removed", {
+  testing <- data.frame(text =
+                          "<i> The review </i>. text with </br> a page break.<b>tag without spaces</b>.<font color='#9900FF'> color </font>")
+
+  results <- token_transcript(testing)
+
+  expect_identical(results[[1]], c("the", "review", "text", "with", "a", "page", "break",
+                                   "tag", "without", "spaces", "color"))
+
+})
