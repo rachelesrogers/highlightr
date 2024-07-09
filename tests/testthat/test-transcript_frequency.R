d6ed8f8efa9a4814f79a143c6fca92c36b63eafd
rachelrogers-gbr <rachel.rogers@huskers.unl.edu>
2024-07-09 15:09:45 -0500

    viewing rendered output

--- tests/testthat/test-transcript_frequency.R
+++ tests/testthat/test-transcript_frequency.R
@@ -10,3 +10,16 @@ test_that("size 2 collocation works", {
   expect_identical(transcript_test$Freq, c(2,3,5,6))
 
 })
+
+test_that("removing html tags works", {
+  transcript_test <- data.frame(text="<i>This <\\i> <b>is</b> a<br> test.")
+  collocation_test <- data.frame(word_number=1:4, col_1=c(2,4,6, NA), col_2=c(NA, 2, 4, 6),
+                                 to_merge = c("this", "is", "a", "test"),
+                                 collocation= c("this is", "is a", "a test", NA))
+  transcript_test <- transcript_frequency(transcript_test, collocation_test)
+
+  expect_identical(dim(transcript_test), c(4L, 14L))
+
+  expect_identical(transcript_test$Freq, c(2,3,5,6))
+
+})
