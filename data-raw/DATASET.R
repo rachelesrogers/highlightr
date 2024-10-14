e6cdc1e3a4fc692cb1719ea8009f3d7bffd03bb5
rachelrogers-gbr <rachel.rogers@huskers.unl.edu>
2024-10-14 10:36:55 -0500

    removing non-ascii characters from dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -6,10 +6,14 @@ comment_full<- read_csv("~/PhD Research/ResponseType_Survey/hybrid_response_data
 comment_example <- comment_full %>% filter(page_count==2 & page_notes !="") %>%
   select(clean_prints, page_notes) %>% rename("ID"="clean_prints", "Notes"="page_notes")
 
+comment_example <- purrr::map_df(comment_example, ~stringi::stri_trans_general(.x, "latin-ascii"))
+
 usethis::use_data(comment_example, overwrite = TRUE)
 
 transcript_full <- read_csv("~/PhD Research/ResponseType_Survey/Combined_Testimony_Formatted.csv")
 
 transcript_example <- transcript_full %>% filter(Page==2) %>% select(Text)
+
+transcript_example <- purrr::map_df(transcript_example, ~stringi::stri_trans_general(.x, "latin-ascii"))
 
 usethis::use_data(transcript_example, overwrite = TRUE)
